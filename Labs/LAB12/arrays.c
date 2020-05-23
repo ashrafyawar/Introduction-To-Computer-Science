90d8afb42129222ffdf9fbb63fd312aa8228c7a2
Ashraf Yawar <m.ashraf0016@gmail.com>
2020-05-23 14:36:00 +0300

    Add files via upload

---
+++ Labs/LAB12/arrays.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<stdlib.h>
+	int main()
+{ 
+	int alinanurunekle(int alimurunkod[],int alimurunmiktari[];int alimurunfoyati,int alimay,int alimsayi)
+{
+	printf("urunkodugiriniz :");
+	scanf("%d",&alimurunkod[alimsayi]);
+		printf("alimmiktarini giriniz:");
+	scanf("%d",&alimurunmiktari[alimsayi]);
+			printf("birimfiyatini giriniz:");
+	scanf("%d",&alimurunfiyati[alimsayi]);
+			printf("urun aliminin yaptigi ayi giriniz:");
+	scanf("%d",&alimay[alimsayi]);
+	 return alimsayi + 1;
+}	
+	int ay toplam giderbul(int ay,int alimay[],int alimurunmiktari[],int alimurunfiyat[],int alimsayi);
+	int urunkarbul(int alimurunkod[],nt alimurunmiktar[],int liamurunfiyati[],int satimurunkod[],int satimurunmiktar[],int     		      satimurunfiyati[],alimsayi,satimsayi)
+	int urunstokbul(int alimurunkod[],int alimurunkimtar[], int satimurunkod[],int satimurunmiktari[],int alimsayi,int satimsayi);
+	return 0;
+}
+}
